541d11a65a3fbbd9ce253e3bd9adca397a37049f
yusama125718 <98173828+yusama125718@users.noreply.github.com>
2025-08-06 21:18:22 +0900

    MySQLのコネクションを使い回すように

--- src/main/java/yusama125718/man10GambleBar/Man10GambleBar.java
+++ src/main/java/yusama125718/man10GambleBar/Man10GambleBar.java
@@ -27,6 +27,7 @@ public final class Man10GambleBar extends JavaPlugin {
     public static VaultAPI vaultapi;
     public static List<Player> remove_players;
     public static List<String> disable_worlds;
+    public static MySQLManager mysql;
 
     private static File shop_folder;
     private static File liquor_folder;
@@ -38,7 +39,7 @@ public final class Man10GambleBar extends JavaPlugin {
         getCommand("mgbar").setExecutor(new Commands());
         mgbar.saveDefaultConfig();
         Thread th = new Thread(() -> {
-            MySQLManager mysql = new MySQLManager(mgbar, "man10_gamble_bar");
+            mysql = new MySQLManager(mgbar, "man10_gamble_bar");
             mysql.execute("create table if not exists bar_shop_log(id int auto_increment,time datetime,liquor_name varchar(20),mcid varchar(16),uuid varchar(36),price integer,buy_id varchar(36),primary key(id))");
             mysql.execute("create table if not exists bar_drink_log(id int auto_increment,time datetime,liquor_name varchar(20),mcid varchar(16),uuid varchar(36),price integer,buy_id varchar(36),win_table varchar(20),primary key(id))");
         });
